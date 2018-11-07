d779d09d4cf653d7cdaec7610e3bf0f4a728b006
user <no@mail.com>
2018-11-07 10:48:42 +0100

    loading script with lua loadfile function.

--- applua.c
+++ applua.c
@@ -43,41 +43,8 @@ int main(int argc, char** argv) {
   init_path[i] = '\0';
   strncpy(init_path+i, "lua.init", 16);
 
-  char * curr_scr = init_path;
-
-  FILE *in = fopen(init_path, "rb");
-  if (!in) {
-
-    char spec_path[length+16];
-    curr_scr = spec_path;
-    strncpy(spec_path, exe_path, length+16);
-    i = back_search(spec_path, ".", length-1);
-    if (!(i > 0)) i = length;
-    strncpy(spec_path+i, ".lua", 16);
-
-    in = fopen(spec_path, "rb");
-    if (!in) {
-
-      fprintf(stderr, "Can not open neither %s or %s\n", init_path, spec_path);
-      return -1;
-
-    }
-  }
-  fseek(in, 0, SEEK_END);
-  long size = ftell(in);
-  if (fseek(in, 0, SEEK_SET)) {
-    fprintf(stderr, "Can not access %s (%d): %s\n", curr_scr, errno, strerror(errno));
-    return -1;
-  }
-
-  char script[size+1];
-  if (1 != fread(script, size, 1, in)) {
-    fprintf(stderr, "Can not read %s (%d): %s\n", curr_scr, errno, strerror(errno));
-    return -1;
-  }
-  script[size] = 0;
-
-  fclose(in);
+  char script[strlen(init_path)+32];
+  snprintf(script, sizeof(script), "(loadfile[[%s]])()\n", init_path);
 
   return luamain_exec(L, script, __FILE__, __LINE__);
 }
