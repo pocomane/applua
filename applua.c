951abbcdf58652cc348a9fa32e61d7175c568f58
user <no@mail.com>
2018-11-12 13:17:56 +0100

    FIX fallback to script with the same name as the executable.

--- applua.c
+++ applua.c
@@ -10,12 +10,12 @@ static int back_search(const char * str, const char * chs, int pos){
   int found = 0;
   int i;
   if (pos < 0) pos = strlen(str);
-  for (i = pos; !found && i > 0; i--)
+  for (i = pos; !found && i >= 0; i--)
     for (int j = 0; !found && chs[j] != '\0'; j += 1)
       if (str[i] == chs[j])
         found = 1;
-  if (found) i += 1;
-  return i;
+  if (found) return i + 1;
+  return pos+1;
 }
 
 int main(int argc, char** argv) {
@@ -36,15 +36,27 @@ int main(int argc, char** argv) {
     lua_setglobal(L, "whereami");
   }
   length = strlen(exe_path);
-
   char init_path[length+16];
-  strncpy(init_path, exe_path, length+16);
-  int i = 1 + back_search(init_path, "/\\", length-1);
-  init_path[i] = '\0';
-  strncpy(init_path+i, "lua.init", 16);
 
-  char script[strlen(init_path)+32];
-  snprintf(script, sizeof(script), "(loadfile[[%s]])()\n", init_path);
+  strncpy(init_path, exe_path, length+16);
+  int i = back_search(init_path, ".", length-1);
+  strncpy(init_path+i, ".lua", 4);
+  i = back_search(init_path, "/\\", length-1);
+  char sp = init_path[i];
+  init_path[i] = '\0';
+
+#define SCRIPT \
+    "local pa, pb = [[%s%clua.init]], [[%s%c%s]]"     "\n"\
+    "local _, ea = io.open(pa,[[r]])"                 "\n"\
+    "if not ea then return loadfile(pa)() end"        "\n"\
+    "local _, eb = io.open(pb,[[r]])"                 "\n"\
+    "if not eb then return loadfile(pb)() end"        "\n"\
+    "error('Can not open script:\\n'..ea..'\\n'..eb)" "\n"\
+""
+
+  char script[sizeof(SCRIPT)+2*length+32];
+  snprintf(script, sizeof(script), SCRIPT,
+    init_path, sp, init_path, sp, init_path+i+1);
 
   return luamain_exec(L, script, __FILE__, __LINE__);
 }
